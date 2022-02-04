5df86132a8136299f3e68999a451f2190cb73056
Satya Prakash <73766765+satyapsr13@users.noreply.github.com>
2022-02-04 09:32:32 -0500

    Time: 361 ms (46.80%), Space: 123.7 MB (97.75%) - LeetHub

--- 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.cpp
+++ 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.cpp
@@ -1,26 +1,38 @@
+// class Solution {
+// public:
+//     vector<int> rearrangeArray(vector<int>& nums) {
+//         queue<int>a1;
+//         queue<int>a2;
+//         for(auto &it:nums)
+//         {
+//             if(it>0)
+//                 a1.push(it);
+//             else a2.push(it);
+//         }
+        
+//         vector<int>ans;
+//         while(!a1.empty())
+//         {
+//             int pos=a1.front();
+            
+//             int neg=a2.front();
+//             ans.push_back(pos);
+//             ans.push_back(neg);
+//             a1.pop();
+//             a2.pop();
+//         }
+//             return ans;
+//     }
+// };
 class Solution {
 public:
     vector<int> rearrangeArray(vector<int>& nums) {
-        queue<int>a1;
-        queue<int>a2;
-        for(auto &it:nums)
-        {
-            if(it>0)
-                a1.push(it);
-            else a2.push(it);
+        vector<int> res(nums.size());
+		 int pos = 0,neg = 1;
+        for(int &num : nums){
+		    if(num > 0) res[pos] = num, pos += 2;
+            else res[neg] = num, neg += 2;
         }
-        
-        vector<int>ans;
-        while(!a1.empty())
-        {
-            int pos=a1.front();
-            
-            int neg=a2.front();
-            ans.push_back(pos);
-            ans.push_back(neg);
-            a1.pop();
-            a2.pop();
-        }
-            return ans;
+        return res;
     }
 };
