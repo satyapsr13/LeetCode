935a910857c6aae8134385153e333093e5d8c890
Satya Prakash <73766765+satyapsr13@users.noreply.github.com>
2022-08-04 15:39:29 -0400

    Time: 12 ms (74.40%), Space: 7.8 MB (46.87%) - LeetHub

--- 264-ugly-number-ii/264-ugly-number-ii.cpp
+++ 264-ugly-number-ii/264-ugly-number-ii.cpp
@@ -1,64 +1,25 @@
-// #define int long long
 class Solution {
-    
-    unordered_map<long long,bool>mp;
 public:
     int nthUglyNumber(int n) {
         
-        vector<long long>v; 
-        v.push_back(1);
-        priority_queue<long long,vector<long long>,greater<long long>>pq;
-        pq.push(1);
-        mp[1]=true;
-        int tt=1690;
-        int t=1;
-        long long i=2LL;
-        while(!pq.empty() and tt--   )
+        int t1=0,t2=0,t3=0;
+        
+        
+        vector<int>v(n+1);
+        v[0]=1;
+        
+        for(int i=1;i<n;++i)
         {
-            long long a= pq.top();
-            pq.pop();
-             i=2 ;
-                
-            // cout<<++t<<"  "<<a  <<endl;
-       
-            // cout<<a<<endl;
-            if(a>2123366400)
-                break;
-            if(!mp[a*i])
-            {
-                
-           
-            // cout<<++t<<"  "<<a<<" * "<<i<<" = " << a*i <<endl;
-                pq.push(a*i);
-                mp[a*i]=1;
-                v.push_back(a*i);
-                
-            }
-            i=3 ;
-            if(!mp[a*i])
-            {
-                
+            v[i]=min({v[t1]*2,v[t2]*3,v[t3]*5});
             
-            // cout<<++t<<"  "<<a<<" * "<<i<<" = "<<a*i <<endl;
-                pq.push(a*i);
-                mp[a*i]=1;
-                v.push_back(a*i);
-            }
-            i=5 ;
+            if(v[i]==v[t1]*2)t1++;
             
-            if(!mp[a*i])
-            {
-            pq.push(a*i);
-                mp[a*i]=1;
-                v.push_back(a*i);
-            }
-             
+            if(v[i]==v[t2]*3)t2++;
+            if(v[i]==v[t3]*5)t3++;
+            
+            
             
         }
-        // cout<<" ";
-        // for(int i=0;i<v.size();++i)
-        //   cout<<v[i]<<" ";
-        sort(v.begin(),v.end());
         return v[n-1];
     }
 };
