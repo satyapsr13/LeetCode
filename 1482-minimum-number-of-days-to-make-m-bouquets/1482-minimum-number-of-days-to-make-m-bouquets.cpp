dd03f60ca4943cd027976c0f94e386b298a5dd18
Satya Prakash <73766765+satyapsr13@users.noreply.github.com>
2022-08-05 02:51:11 -0400

    Time: 314 ms (31.99%), Space: 63.3 MB (65.81%) - LeetHub

--- 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.cpp
+++ 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.cpp
@@ -1,53 +1,66 @@
 class Solution {
-    bool check(vector<int>&v,int m,int k,int mid)
-    {
-        int n=v.size();
+    int n;
+    bool find(vector<int>&v,int m,int k,int mid)
+    {  
         int count=0;
-        int total=0;
+        int sum=0;
         for(int i=0;i<n;++i)
-        {   
-            if(v[i]<=mid)
-            { count++;
-             if(count==k)
-             {count=0;
-              total++;
-              if(total==m)return 1;
-                 
-             }
-                
-            }else{
+        {
+            if(v[i]>mid)
+            {
                 count=0;
-                
+                continue;
+            }
+            count++;
+            if(count==k)
+            {
+                sum++;
+                count=0;
             }
             
             
-            
         }
-    
-        return false;
+                    // cout<<mid<<" "<<sum<<endl;
+        
+        
+        return sum>=m;
+        
+        
     }
+    
 public:
     int minDays(vector<int>& bloomDay, int m, int k) {
-     
-        int n=bloomDay.size();
+         n=bloomDay.size();
+        
         if(n<m*k)return -1;
         
-        int mx=*max_element(bloomDay.begin(),bloomDay.end());
-        int l=1,r=mx;
-        int ans;
+        int mx=INT_MIN,mn=INT_MAX;
+        for(int i=0;i<n;++i)
+        
+        {
+            mx=max(mx,bloomDay[i]);
+            mn=min(mn,bloomDay[i]);
+            
+        }
+        int l=mn;
+        int r=mx;
+        
+        int ans=-1;
+        
         while(l<=r)
         {
             int mid=l+(r-l)/2;
-             if(check(bloomDay,m,k,mid))
-             { ans=mid;
-              r=mid-1;
-              
-                 
-             }else{
-                 l=mid+1;
-                 ans=l;
-                 
-             }
+            
+
+            if(find(bloomDay,m,k,mid))
+            {ans=mid;
+             r=mid-1;
+                
+            }else{
+                l=mid+1;
+                ans=l;
+                
+            }
             
             
         }
