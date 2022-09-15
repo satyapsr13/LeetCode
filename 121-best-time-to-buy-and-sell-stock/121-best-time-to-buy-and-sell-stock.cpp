f15a0544535c84fe6e8e41d1d0a6ee947f89bda2
Satya Prakash <73766765+satyapsr13@users.noreply.github.com>
2022-09-15 06:44:23 -0700

    Time: 313 ms (9.30%), Space: 93.3 MB (89.21%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.cpp
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.cpp
@@ -1,42 +1,25 @@
 class Solution {
-    int n;
-    // int dp[100001][2]={-1};
-    vector<vector<int>>dp;
-    int find(vector<int>&v,int i,bool buy)
-    {
-        
-        if(i==n-1 )
-        {
-            if(!buy)
-                return v[i];
-            
-            return 0;
-        }
-        // cout<<i<<" "<<buy<<endl;
-        if(dp[i][buy]!=-1)
-            return dp[i][buy];
-     
-        if(buy)
-        {
-            return dp[i][buy]=max(find(v,i+1,0)-v[i],find(v,i+1,1));
-        }else{
-            return dp[i][buy]=max(v[i],find( v,i+1,0 ));
-        
-        }
-        
-        
-    }
-    
 public:
     int maxProfit(vector<int>& prices) {
-         n=prices.size();
+        int n=prices.size();
         
-        dp.resize(n,vector<int>(2,-1) );
-      
-       
-        
-        return find(prices,0,1);
-        
-        
+        int last=INT_MAX;
+        int ans=0;
+        for(int i=0;i<n;++i)
+        {
+            
+            if(prices[i]<last)
+            {
+                last=prices[i];
+            }
+            int temp=prices[i]-last;
+            
+            if(temp>ans)
+            {
+                ans=temp;
+            }
+            
+        }
+        return ans;
     }
 };
