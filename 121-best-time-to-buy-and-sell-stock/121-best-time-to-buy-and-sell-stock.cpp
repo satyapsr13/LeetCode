31cc92a06a238c1a12c54dc555419d194d2b228d
Satya Prakash <73766765+satyapsr13@users.noreply.github.com>
2022-09-15 06:11:42 -0700

    Time: 541 ms (5.06%), Space: 161.3 MB (5.18%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.cpp
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.cpp
@@ -1,25 +1,42 @@
 class Solution {
-    int dp[100005][2] ;
-     int n;
-    int find(vector<int>&v,int i,bool buy,int k)
+    int n;
+    // int dp[100001][2]={-1};
+    vector<vector<int>>dp;
+    int find(vector<int>&v,int i,bool buy)
     {
-        if( n==1|| i==n  || k==0)return 0;
-        if(dp[i][buy]!=-1)return dp[i][buy];
+        
+        if(i==n-1 )
+        {
+            if(!buy)
+                return v[i];
+            
+            return 0;
+        }
+        // cout<<i<<" "<<buy<<endl;
+        if(dp[i][buy]!=-1)
+            return dp[i][buy];
+     
         if(buy)
         {
-            
-            return dp[i][buy]= max(-v[i]+find(v,i+1,!buy,k ),find(v,i+1,buy,k));
+            return dp[i][buy]=max(find(v,i+1,0)-v[i],find(v,i+1,1));
         }else{
-            
-            return dp[i][buy]= max(v[i],find(v,i+1,buy,k));
-      
+            return dp[i][buy]=max(v[i],find( v,i+1,0 ));
+        
         }
+        
+        
     }
     
 public:
     int maxProfit(vector<int>& prices) {
-        n=prices.size();
-       memset(dp,-1,sizeof(dp));
-        return max(0,find(prices,0,1,1));
+         n=prices.size();
+        
+        dp.resize(n,vector<int>(2,-1) );
+      
+       
+        
+        return find(prices,0,1);
+        
+        
     }
 };
